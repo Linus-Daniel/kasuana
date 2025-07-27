9abdd02450599c73e8bfe6a009267e0ecef79b81
Linus-Daniel <vandulinus@gmail.com>
2025-07-27 14:56:40 +0100

    new

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "Kasuana Amin",
+  description: "Building Africa's Grassroots Trade Network",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className=" antialiased">
+        <div className="h-screen">{children}</div>
+      </body>
+    </html>
+  );
+}
