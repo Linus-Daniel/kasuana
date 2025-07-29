855fe506ce6740a07a4569bfdb64edaeb62b0686
Linus-Daniel <vandulinus@gmail.com>
2025-07-29 08:18:40 +0100

    new

--- src/model/Vendor.ts
+++ src/model/Vendor.ts
@@ -1,66 +1,58 @@
-import  { Schema, Document,model, models } from 'mongoose';
+import { Schema, Document, model, models } from "mongoose";
 
 export interface IVendor extends Document {
   name: string;
   email: string;
   phone: string;
   address: string;
-  description:string;
-  image:string;
+  story: string;
+  image: string;
   verified: boolean;
-    socialLinks: {
-        facebook?: string;
-        whatsapp?: string;
-        linkedin?: string;
-
-    };  
   createdAt: Date;
   updatedAt: Date;
 }
 
-const VendorSchema = new Schema<IVendor>({
+const VendorSchema = new Schema<IVendor>(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: false,
-        unique: true,
-        trim: true,
-        lowercase: true
+      type: String,
+      required: false,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
-    description: {
-        type: String,
-        required: true,
-        trim: true
+    story: {
+      type: String,
+      required: true,
+      trim: true,
     },
     image: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     verified: {
-        type: Boolean,
-        default: false
-    },
-    socialLinks: {
-        facebook: { type: String, trim: true },
-        whatsapp: { type: String, trim: true },
-        linkedin: { type: String, trim: true }
+      type: Boolean,
+      default: false,
     },
     phone: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     address: {
-        type: String,
-        required: true,
-        trim: true
-    }
-},{timestamps:true})
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const Vendor = models.Vendor || model<IVendor>("Vendor", VendorSchema);
 export default Vendor;
